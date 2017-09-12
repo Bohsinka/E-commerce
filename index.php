c99a29f7d25b73d287ab483285343efe0abd76ed
Bohsinka <31533564+Bohsinka@users.noreply.github.com>
2017-09-12 14:13:38 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,36 +1,33 @@
-
 <?php
+//Initialisation de la session
 session_start();
 
+//Initialisation de la BDD
 $db = new PDO('mysql:host=localhost;dbname=Commerce', 'root', 'troiswa', [
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 	]);
 $db->exec('SET NAMES UTF8');
 
+//Chargement des classes
 function __autoload($class_name){
 	require('apps/'.$class_name.'.class.php');
 }
-
+	
+//Gestion des pages
 $page = 'home';
 $access = ["home", "register", "single", "create", "profile"];
-if (isset($_GET['page']))
-{
+if (isset($_GET['page'])){
 	if (in_array($_GET['page'], $access))
-	{
 		$page = $_GET['page'];
-	}
-	else
-	{
+	else{
 		header('Location: index.php');
 		exit();
 	}
 }
-
+//Chargement des traitements en fonction de la page en cours
 $accessTraitement = ["home" => "User-traitement", "register" => "User-traitement", "profile" => "User-traitement", "create" => "Product-traitement"];
-if (isset($accessTraitement[$page]))
-{
+if ( isset($accessTraitement[$page]) )
 	require('apps/traitement/'.$accessTraitement[$page].'.php');
-}
 
 require('apps/skel.php');
 ?>
